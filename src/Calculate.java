a5b2eedc67273d0cd04cebd988afd8227ef92bf6
MukhitIskandarov <mukhitiskandarov@gmail.com>
2015-10-20 00:17:49 +0600

    Change Calculate and create calculator

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by Мухит on 19.10.2015.
  */
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("args = [" + args + "]");
+        Scanner scan = new Scanner(System.in);
+        try{
+            Calculator calculator = new Calculator();
+            String  exit = "no";
+            while (!exit.equals("yes")){
+                System.out.println("Enter first arg : ");
+                String first = scan.next();
+                System.out.println("Enter second arg : ");
+                String second = scan.next();
+                calculator.add(Integer.valueOf(first), Integer.valueOf(second));
+                System.out.println("Result = " + calculator.getResult());
+                calculator.cleanResult();
+                System.out.println("Exit : yes/no");
+                exit = scan.next();
+            }
+        }
+        finally {
+            scan.close();
+        }
     }
 }
